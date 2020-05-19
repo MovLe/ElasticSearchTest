38eba488649692d4f8cc5a24a4c50e7a4032d402
Movle <richard.xiu.520@gmail.com>
2020-05-19 11:42:03 +0800

    新建ElasticSearch项目，

---
+++ src/main/java/ES/ESTest.java
@@ -0,0 +1,18 @@
+package ES;
+
+/**
+ * @ClassName ESTest
+ * @MethodDesc: TODO ESTest功能介绍
+ * @Author Movle
+ * @Date 5/19/20 11:40 上午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+
+
+public class ESTest {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
